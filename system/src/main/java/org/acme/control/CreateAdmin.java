f704cbb196fe01a096ea4c485b9059a2714a1dbb
Caroline Scherer <62570985+carolineschererr@users.noreply.github.com>
2022-07-07 21:15:01 -0300

    Update CreateAdmin.java

--- system/src/main/java/org/acme/control/CreateAdmin.java
+++ system/src/main/java/org/acme/control/CreateAdmin.java
@@ -4,6 +4,7 @@ public class CreateAdmin {
     
     private String username;
     private String password;
+    private String email;
 
 
     public String getUsername() {
@@ -18,5 +19,11 @@ public class CreateAdmin {
     public void setPassword(String password) {
         this.password = password;
     }
+    
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
 
 }
